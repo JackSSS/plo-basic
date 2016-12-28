4496cca9569fd5fdd8598c1eb4e00d95a20c7088
Jack Sneed <JackSSS@users.noreply.github.com>
2016-12-27 17:42:20 -0800

    update mobile

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,17 +1,17 @@
 $( document ).ready(function() {
-  $('#home-link').on('click', function() {
+  $('#home-link, #home-button').on('click', function() {
     $('#portfolio, #skills, #contact').css('display', 'none');
     $('#welcome').css('display', 'block');
   });
-  $('#portfolio-link').on('click', function() {
+  $('#portfolio-link, #portfolio-button').on('click', function() {
     $('#welcome, #skills, #contact').css('display', 'none');
     $('#portfolio').css('display', 'block');
   });
-  $('#skills-link').on('click', function() {
+  $('#skills-link, #skills-button').on('click', function() {
     $('#welcome, #portfolio, #contact').css('display', 'none');
     $('#skills').css('display', 'block');
   });
-  $('#contact-link').on('click', function() {
+  $('#contact-link, #contact-button').on('click', function() {
     $('#welcome, #portfolio, #skills').css('display', 'none');
     $('#contact').css('display', 'block');
   });
